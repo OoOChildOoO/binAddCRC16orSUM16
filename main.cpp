7105dc34f7a7685eb7e40f4a788590f2755bfe31
WJ <240997293@qq.com>
2022-01-21 15:55:03 +0800

    Signed-off-by: WJ <240997293@qq.com> 这是一个给单片机烧录用的.bin文件添加 CRC16校验/SUM16校验的QT程序

---
+++ main.cpp
@@ -0,0 +1,13 @@
+﻿#include "mainwindow.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.setWindowIcon(QIcon(":blue.png"));
+    w.setWindowTitle("binAppendCRC16/SUM16(limit4MB)");
+    w.show();
+    return a.exec();
+}
---
+++ mainwindow.h
@@ -0,0 +1,33 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+#define FILE_SIZE_MAX (1024 * 1024 * 4 +2)
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+    unsigned int fun_getFileSource(char *dst);
+    void fun_saveToFile(QString filePath, char * buf, quint64 len);
+    quint16 GetCRC16(char *puchMsg,unsigned int usDataLen);
+    quint16 GetSUM16(char *puchMsg,unsigned int usDataLen);
+
+private slots:
+    void on_pushButton_chooseFile_clicked();
+
+    void on_pushButton_save_clicked();
+
+private:
+    Ui::MainWindow *ui;
+    QList<QString> mCheckList;
+};
+#endif // MAINWINDOW_H
